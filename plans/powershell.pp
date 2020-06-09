86b110bcc8c76311e344f235e94b8d96b90be61d
Bill Hurt <hurt.bill@gmail.com>
2020-06-09 06:39:38 -0700

    upload rsa key

--- plans/powershell.pp
+++ plans/powershell.pp
@@ -1,8 +1,9 @@
 plan dev_provisioner::powershell(
   TargetSpec $targets,
-  Optional[String] $repo,
+  # Optional[String] $repo,
 ) {
   run_task('dev_provisioner::choco_install', $targets)
   run_task('dev_provisioner::choco_install_packages', $targets)
   run_task('dev_provisioner::add_hub_alias', $targets)
+  upload_file('c:/users/Bill Hurt/.ssh/id_rsa', 'c:/users/administrator/.ssh/id_rsa')
 }
